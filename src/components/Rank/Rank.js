43d7923c1d185bc836cf8114fb7453ee7edb2581
Adam Love <adam.love127@gmail.com>
2018-08-21 15:42:53 +1000

    Update register & sign in components

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Rank.css'
 
-const Rank = () => {
+const Rank = ({name, entries}) => {
   return (
   <div>
     <div className='RankText'>
-      {`Adam, your current rank is...`}
+      {`${name}, your current entry count is...`}
     </div>
     <div className='RankText--large'>
-      {`#5`}
+      {entries}
     </div>
   </div>
   )
